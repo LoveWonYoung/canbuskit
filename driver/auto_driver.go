ff99049ea53ad43e152979b73a5597765dd72c15
liaolianmin <168514731+LoveWonYoung@users.noreply.github.com>
2026-01-29 17:34:56 +0800

    Update auto_driver.go

--- driver/auto_driver.go
+++ driver/auto_driver.go
@@ -9,8 +9,6 @@ import (
 	"log"
 	"strings"
 	"sync"
-
-	"github.com/LoveWonYoung/canbuskit/driver"
 )
 
 // AutoDriver selects the first available CAN device driver.
@@ -39,7 +37,7 @@ func (a *AutoDriver) Init() error {
 		{name: "Toomoss CAN 1 500K 2M", driver: NewToomoss(a.canType, CHANNEL1)},
 		{name: "TSMaster CAN 1 500K 2M", driver: NewTSMaster(a.canType)},
 		{name: "PCAN CAN 1 500K 2M", driver: NewPCAN(a.canType, CHANNEL1)},
-		{name: "Vector", driver: NewVector(a.canType,driver.CANOEVN1640,driver.CHANNEL4)},
+		{name: "Vector", driver: NewVector(a.canType,CANOEVN1640,CHANNEL4)},
 	}
 
 	var errs []string
